ec5fa5f0220967bd8a838ccbab964629cf03588f
vojta.krasa@gmail.com <vojta.krasa@gmail.com@428bcac6-8cb2-11dd-b496-dd7b60ab8c28>
2013-04-03 23:37:32 +0000

    popup shortcuts, CamelCase to words

--- trunk/src/main/java/osmedile/intellij/stringmanip/AbstractStringManipAction.java
+++ trunk/src/main/java/osmedile/intellij/stringmanip/AbstractStringManipAction.java
@@ -1,72 +1,78 @@
 package osmedile.intellij.stringmanip;
 
+import osmedile.intellij.stringmanip.utils.StringUtils;
+
 import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.editor.SelectionModel;                         
-import com.intellij.openapi.editor.actionSystem.EditorAction;              
-import com.intellij.openapi.editor.actionSystem.EditorWriteActionHandler;  
-import osmedile.intellij.stringmanip.utils.StringUtils;                    
-                                                                    
-/**                                                                    
- * @author Olivier Smedile                                                 
+import com.intellij.openapi.editor.SelectionModel;
+import com.intellij.openapi.editor.actionSystem.EditorAction;
+import com.intellij.openapi.editor.actionSystem.EditorWriteActionHandler;
+
+/**
+ * @author Olivier Smedile
  * @version $Id: AbstractStringManipAction.java 62 2008-04-20 11:11:54Z osmedile $
- */                                                                    
-public abstract class AbstractStringManipAction extends EditorAction {     
-                                                                    
-    protected AbstractStringManipAction() {                                
-        super(null);                                                       
-                                                                    
-        this.setupHandler(new EditorWriteActionHandler() {                 
-            public void executeWriteAction(Editor editor, DataContext dataContext) {
-                final SelectionModel selectionModel = editor.getSelectionModel();
-                String selectedText = selectionModel.getSelectedText();    
+ */
+public abstract class AbstractStringManipAction extends EditorAction {
 
-                boolean allLinSelected = false;
-                if (selectedText == null) {
-                    selectionModel.selectLineAtCaret();
-                    selectedText = selectionModel.getSelectedText();
-                    allLinSelected = true;
+	protected AbstractStringManipAction() {
+		this(true);
+	}
 
-                    if (selectedText == null) {
-                        return;
-                    }
-                }
-                String[] textParts = selectedText.split("\n");
-                if (selectionModel.hasBlockSelection()) {
-                    int[] blockStarts = selectionModel.getBlockSelectionStarts();
-                    int[] blockEnds = selectionModel.getBlockSelectionEnds();
+	protected AbstractStringManipAction(boolean setupHandler) {
+		super(null);
+		if (setupHandler) {
+			this.setupHandler(new EditorWriteActionHandler() {
+				public void executeWriteAction(Editor editor, DataContext dataContext) {
+					final SelectionModel selectionModel = editor.getSelectionModel();
+					String selectedText = selectionModel.getSelectedText();
 
-                    int plusOffset = 0;
+					boolean allLinSelected = false;
+					if (selectedText == null) {
+						selectionModel.selectLineAtCaret();
+						selectedText = selectionModel.getSelectedText();
+						allLinSelected = true;
 
-                    for (int i = 0; i < textParts.length; i++) {
-
-                        String newTextPart = transform(textParts[i]);
-                        if (allLinSelected) {
-                            newTextPart += "\n";
-                        }
-
-                        editor.getDocument().replaceString(blockStarts[i] + plusOffset,
-                                blockEnds[i] + plusOffset, newTextPart);
-
-						int realOldTextLength = blockEnds[i] - blockStarts[i];
-						plusOffset += newTextPart.length() - realOldTextLength;
+						if (selectedText == null) {
+							return;
+						}
 					}
-                } else {
-                    for (int i = 0; i < textParts.length; i++) {
-                        textParts[i] = transform(textParts[i]);
-                    }
+					String[] textParts = selectedText.split("\n");
+					if (selectionModel.hasBlockSelection()) {
+						int[] blockStarts = selectionModel.getBlockSelectionStarts();
+						int[] blockEnds = selectionModel.getBlockSelectionEnds();
 
-                    final String s = StringUtils.join(textParts, '\n');
-                    editor.getDocument().replaceString(selectionModel.getSelectionStart(),
-                            selectionModel.getSelectionEnd(), s);
-                    if (allLinSelected) {
-                        editor.getDocument().insertString(selectionModel.getSelectionEnd(), "\n");
-                    }
-                }
-            }
-        });
-    }
+						int plusOffset = 0;
 
+						for (int i = 0; i < textParts.length; i++) {
 
-    public abstract String transform(String s);
+							String newTextPart = transform(textParts[i]);
+							if (allLinSelected) {
+								newTextPart += "\n";
+							}
+
+							editor.getDocument().replaceString(blockStarts[i] + plusOffset, blockEnds[i] + plusOffset,
+									newTextPart);
+
+							int realOldTextLength = blockEnds[i] - blockStarts[i];
+							plusOffset += newTextPart.length() - realOldTextLength;
+						}
+					} else {
+						for (int i = 0; i < textParts.length; i++) {
+							textParts[i] = transform(textParts[i]);
+						}
+
+						final String s = StringUtils.join(textParts, '\n');
+						editor.getDocument().replaceString(selectionModel.getSelectionStart(),
+								selectionModel.getSelectionEnd(), s);
+						if (allLinSelected) {
+							editor.getDocument().insertString(selectionModel.getSelectionEnd(), "\n");
+						}
+					}
+				}
+			});
+		}
+
+	}
+
+	public abstract String transform(String s);
 }
